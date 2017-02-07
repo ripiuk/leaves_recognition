494fd259903346b9ff0ebac9218b793d4191a0dd
Sany <ripiuk@ua.fm>
2017-02-07 23:17:43 +0200

    change stat

--- leaves_recognition/settings.py
+++ leaves_recognition/settings.py
@@ -126,7 +126,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
